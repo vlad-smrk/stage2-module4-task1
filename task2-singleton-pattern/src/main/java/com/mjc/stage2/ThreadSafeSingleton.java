5907229bbfc0c3ee5f3adff45dfc00bc4ed6113e
Vladislav Samorukov <smrk.vld@gmail.com>
2023-04-26 23:57:38 +0300

    done

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,5 +1,11 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    // Write your code here!
+    private static final ThreadSafeSingleton instance = new ThreadSafeSingleton();
+
+    private ThreadSafeSingleton() {}
+
+    public static ThreadSafeSingleton getInstance() {
+        return instance;
+    }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,41 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+
+    private static String driver;
+    private static String url;
+    private static String username;
+    private static String password;
+
+    static {
+        try (InputStream config = H2ConnectionFactory.class.getClassLoader().getResourceAsStream("h2database.properties")) {
+            Properties properties = new Properties();
+            properties.load(config);
+            driver = properties.getProperty("jdbc_driver");
+            url = properties.getProperty("db_url");
+            username = properties.getProperty("user");
+            password = properties.getProperty("password");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Connection createConnection() {
+        try {
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
